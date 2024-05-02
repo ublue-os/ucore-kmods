576b3c9567f4277c9fa3848f67283e9579e32f27
Benjamin Sherman <benjamin@holyarmy.org>
2024-05-02 09:08:08 -0500

    remove the tar-hack stuff

--- tar-hack-script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-echo "'tar' was called with these options: $@"
-exit 20
-#tar -xvf -options $@
--- tar-hack-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-set -oeux pipefail
-
-mv /usr/bin/tar /usr/bin/tar-actual
-mv /tmp/tar-hack-script.sh /usr/bin/tar
-chmod 755 /usr/bin/tar
